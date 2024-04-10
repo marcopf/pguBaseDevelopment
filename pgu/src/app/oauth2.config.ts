edb252143eed46a13fd23bf2a074fd1ba6bc0510
marco <marco.paternosto@outlook.it>
2024-04-10 12:04:21 +0200

    lot of changes

--- pgu/src/app/oauth2.config.ts
+++ pgu/src/app/oauth2.config.ts
@@ -2,7 +2,7 @@ import { AuthConfig } from 'angular-oauth2-oidc';
 
 export const authCodeFlowConfig: AuthConfig = {
   // Url of the Identity Provider
-  issuer: 'http://localhost:8080/realm/master',
+  issuer: 'http://localhost:8080/realms/master',
 
   // URL of the SPA to redirect the user to after login
   redirectUri: 'http://localhost:4200',
